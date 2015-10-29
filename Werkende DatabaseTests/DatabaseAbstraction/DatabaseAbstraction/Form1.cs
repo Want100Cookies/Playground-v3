bfbb3cbd9c7f0cb12100cc7a33919f5672599483
Wout Sanders <wsanders@outlook.com>
2015-10-29 12:30:41 +0100

    Added the testing version of the database wrapper class with enhanced features as separate project (further testing needed).

---
+++ Werkende DatabaseTests/DatabaseAbstraction/DatabaseAbstraction/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseAbstraction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Simple example select statement.
+        /// Result: 2 messagebox popups (2 records present).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Specify database type and location.
+            OdbcDbManager db = new OdbcDbManager(OdbcDbManager.DB_SQLITE, @"D:\mydb.db");
+
+            // Fetch all results of the query.
+            DataTable dt = db.select("`name` FROM `test`");
+            
+            // Loop through results.
+            foreach (DataRow row in dt.Rows)
+            {
+                MessageBox.Show(row["name"].ToString());
+            }
+
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            d.Add("@id", "23");
+            d.Add("@name", "haha");
+            db.insert("`test` (``id`,`name`) VALUES(?,?)", d);
+        }
+    }
+}
